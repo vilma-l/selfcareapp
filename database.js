377fb282d24c079d8929d5f7b51166e285566c43
Vilma Laurila <vilma.laurila@myy.haaga-helia.fi>
2023-12-06 12:23:35 +0200

    created routine into database

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import * as SQLite from 'expo-sqlite';
 //database connection
 const db = SQLite.openDatabase('selfcaredb.db');
 
-//creating the table
+//creating the daily table
 db.transaction(tx => {
     tx.executeSql(
         'CREATE TABLE IF NOT EXISTS daily (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT, mood TEXT, sleephours TEXT, sleepquality TEXT);'
@@ -52,3 +52,54 @@ export const getAllDaily = () => {
     });
 };
 
+//creating the routine table
+db.transaction(tx => {
+    tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS routine (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, routinename TEXT, routinestatus TEXT);'
+        );
+    });
+
+//inserting data into the routine table
+export const addRoutine = () => {
+    db.transaction(tx => {
+        tx.executeSql('INSERT INTO routine (routinename, routinestatus) VALUES (?, ?);', [routinename, routinestatus]);
+    }, null, updateList
+    )
+};
+
+//updating routine list
+export const updateRoutineList = () => {
+    db.transaction(tx => {
+        tx.executeSql('SELECT * FROM routine;', [], (_, { rows }) =>
+        setRoutines(rows._array)
+       );
+   });
+};
+
+//deleting item from routine list
+export const deleteRoutine = (id) => {
+    db.transaction(tx => {
+        tx.executeSql(`DELETE FROM routine WHERE id = ?;`, [id]);
+    }, null, updateRoutineList
+   )
+};
+
+//fetching all data from the routine table
+export const getAllRoutines = () => {
+    return new Promise((resolve, reject) => {
+        db.transaction(
+            (tx) => {
+                tx.executeSql('SELECT * FROM routine;', [], (_, result) => {
+                    const data = [];
+                    for (let i = 0; i < result.rows.length; i++) {
+                        data.push(result.rows.item(i));
+                    }
+                    resolve(data);
+                });
+            },
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+};
